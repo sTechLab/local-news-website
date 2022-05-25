ebc8e3c76e6ed0adcad067090f483383f7fca171
Tiffany Chiang <tiffany85221@gmail.com>
2022-05-25 16:37:22 -0400

    update Overview Introduction and Explore Dataset

--- interface/explore_data.py
+++ interface/explore_data.py
@@ -1,22 +1,94 @@
 import streamlit as st
 import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
 
-TOP_N = 20
-OUTLETS_FILEPATH = 'interface/data/outlet_example.csv'
+N = 20
+OUTLET_DATASET_URL = "https://raw.githubusercontent.com/sTechLab/local-news-dataset/main/local_news_outlets_dataset.csv"
+OUTLET_DATASET_FILEPATH = './data/local_news_outlets_dataset.csv'
+STATE_NAMES = [ 'AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
+    'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
+    'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM',
+    'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX',
+    'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']
 
-def get_content_dataframe():
-    selected_columns = ['Name', 'State', 'City', 'WebsiteUrl', 'TwitterHandle', 'FacebookUrl', 'CreatedAt']
-    df = pd.read_csv(OUTLETS_FILEPATH).set_index('OutletId')
-    return df[selected_columns].head(TOP_N)
+def get_content_dataframe(selected_state, selected_ranking):
+    selected_columns = ['Name', 'State', 'City', 'WebsiteUrl', 'TwitterId', 'FacebookId']
+    extra_columns = ['TwitterFollowersCount', 'FacebookSubscriberCount', 'PopulationReach', 'ContentNetworkNeighbors'] 
+    all_df = pd.read_csv(OUTLET_DATASET_FILEPATH).set_index('OutletId')
+    df = all_df.copy()
+    if selected_state != 'All':
+            df = df[df.State == selected_state]
+    if selected_ranking != 'All':
+        if selected_ranking == "Highest Twitter Followers Count":
+            df = df.sort_values(['TwitterFollowersCount', 'State', 'City', 'Name'], ascending=[False, True, True, True])
+        elif selected_ranking == "Highest Facebook Subscribers Count":
+            df = df.sort_values(['FacebookSubscriberCount', 'State', 'City', 'Name'], ascending=[False, True, True, True])
+        elif selected_ranking ==  "Highest Population Reach":
+            df = df.sort_values(['PopulationReach', 'State', 'City', 'Name'], ascending=[False, True, True, True])
+        elif selected_ranking == "Lowest Population Reach":
+            df = df.sort_values(['PopulationReach', 'State', 'City', 'Name'])
+        elif selected_ranking == "Highest Content Network Neighbors":
+            df = df.sort_values(['ContentNetworkNeighbors', 'State', 'City', 'Name'], ascending=[False, True, True, True])
+        elif selected_ranking == "Lowest Content Network Neighbors":
+            df = df.sort_values(['ContentNetworkNeighbors', 'State', 'City', 'Name'])
+    # for printing
+    for col in ['TwitterId', 'FacebookId'] + extra_columns:
+        df[col] = df[col].apply(lambda x: str(int(round(x))) if pd.notnull(x) else x)
+    df = df.fillna('')
+    return df.head(N)[selected_columns + extra_columns]
+
+# def get_network_graph():
+#     G = nx.karate_club_graph()
+#     fig, ax = plt.subplots()
+#     pos = nx.kamada_kawai_layout(G)
+#     nx.draw(G, pos, with_labels=True)
+#     return fig
 
 def get_explore_data_page(shared_state):
     st.header("Explore The Dataset")
 
     st.markdown("""
-        # This is the dataset
-    """)
+### This is the dataset
+what is this dataset
 
-    st.table(get_content_dataframe())
+""")
+    
+    col1, col2 = st.columns(2)
+    selected_state = col1.selectbox(
+        "Select State",
+        ["All"] + sorted(STATE_NAMES) + ['non-US'],
+    )
+    selected_ranking = col2.selectbox(
+        "Rank by",
+        [
+            "All",
+            "Highest Twitter Followers Count",
+            "Highest Facebook Subscribers Count",
+            "Highest Population Reach",
+            "Lowest Population Reach",
+            "Highest Content Network Neighbors",
+            "Lowest Content Network Neighbors"
+        ],
+    )
+
+    st.table(get_content_dataframe(selected_state, selected_ranking))
+
+    st.markdown("""
+### Field Description
+| Field      | Description                                                                                                                                                                      |
+|------------|----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| OutletId   | Unique local news outlet ID of this dataset (from 0 to 10256)                                                                                                                    |
+| Name       | Name of the local news outlet                                                                                                                                                    |
+| State      | Official United States Postal Service (USPS) Code for the States and District of Columbia, listed as “non-US” if outlet location is not in the US and “NA” if location not found |
+| City       | Name of the city where the outlet is located, listed as “non-US” if outlet location is not in the US and “NA” if location not found                                              |
+| WebsiteUrl | Website URL of the local news outlet                                                                                                                                             |
+| TwitterId  | Unique identifier of Twitter account                                                                                                                                             |
+| FacebookId | Unique identifier of Facebook account, also known as platformId on CrowdTangle                                                                                                   |
+""")
+ 
+    # st.pyplot(get_network_graph())
+
 
 
 if __name__ == "__main__":
--- interface/landing_page.py
+++ interface/landing_page.py
@@ -8,34 +8,38 @@ def get_landing_page(shared_state):
         """
         ### Introduction
 
-        ### Abstract
+        On this website, you will find the largest dataset of local news outlets in the U.S. and their social media handles. 
+        This dataset includes a total of 10,257 news outlets. We map 7,859 of them to a location and county, 9,231 to a Facebook account and 5,645 to a Twitter account. 
+        
+        For the outlets that have a Twitter or a Facebook account, we provide the IDs that can be used to retrieve their content from Facebook through the CrowdTangle API and from Twitter using the Twitter API V2. 
+        The FIPS county code makes it easy to retrieve data at the county-level, including census data or voter records. 
+        We constructed this dataset through expanding a seed directory of local news outlets with Facebook’s “Related Pages” feature. 
+        Based on our validation, we expect this dataset to cover approximately 60% of local news outlets in the U.S. 
+        
+        The full methodology for how this dataset was constructed and validated, and a use case for analysis around COVID-19, is detailed in our paper “Understanding Social Local Coverage and Engagement at Scale During the COVID-19 Pandemic,” forthcoming at ICWSM.
 
-        During the COVID-19 pandemic, local news organizations have played an important role in keeping communities informed about the spread and impact of the virus.
-        We explore how political, social media, and economic factors impacted the way local media reported on COVID-19 developments at a national scale between January 2020 and July 2021. 
-        We construct and make available a dataset of over 10,000 local news organizations and their social media handles across the U.S.
-        Building on this data, we analyze how local and national media covered four key COVID-19 news topics: *Statistics and Case Counts*, *Vaccines and Testing*, *Public Health Guidelines*, and *Economic Effects*.
-        Our results show that news outlets with larger population reach reported proportionally more on COVID-19 than more local outlets.
-        Separating the analysis by topic, we expose more nuanced trends, for example that outlets with a smaller population reach covered the *Statistics and Case Counts* topic proportionally more, and the *Economic Effects* topic proportionally less.
-        Our analysis further shows that while people engaged proportionally less with outlets with a smaller population reach, they engaged proportionally more and used stronger reactions for COVID-19 posts.
-        Finally, we demonstrate that COVID-19 posts in Republican-leaning counties generally received more comments and fewer likes than in Democratic counties, perhaps indicating controversy.
+        We additionally expanded this dataset with two implicitly-constructed metrics. 
+        The first, population reach, is a measure of how local or national an outlet is. 
+        Based on a method first proposed by Hagar et al. (2020), we use Twitter audience location data normalized with logged state density to produce our measure of outlet population reach. 
+        Secondly, we also provide our content network neighbors metric, which is the number of other outlets that a given outlet reposts content with. 
+        This allows the identification of outlets that are part of a larger network.
 
-        ### Key Takeaways
-        - first
-        - second
+        This dataset can be used to analyze content of local news outlets in the U.S. at large scales. 
+        We encourage researchers to make use of Twitter and Facebook APIs to retrieve local news social media content.
 
-        ### Privacy and Ethical Considerations
+        ### Contact Us
 
-
+        
         *Marianne Aubin Le Quéré ([@marianneaubin](https://twitter.com/marianneaubin)), Ting-Wei Chiang ([@chiangtingwei](https://twitter.com/chiangtingwei)), Mor Naaman ([@informor](https://twitter.com/informor)).*
 
     """
     )
     
     # st.image(img, use_column_width=True)
-    st.markdown("""
-        ![Figure 1]({})
-        **Figure 1:** This is a CT logo
-    """.format(bucket_image_urls["ct-logo"]))
+    # st.markdown("""
+    #     ![Figure 1]({})
+    #     **Figure 1:** This is a CT logo
+    # """.format(bucket_image_urls["ct-logo"]))
 
 
 if __name__ == "__main__":
